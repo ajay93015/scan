e20a4122dfe5bceff0736f588f060123c6b47bf8
ajay93015 <105852839+ajay93015@users.noreply.github.com>
2025-06-27 16:39:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const fs = require("fs");
+app = express();
+//app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+app.listen(3000)
